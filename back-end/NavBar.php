d9f6efcaff9b1078d5599ba4471865b49e8ae840
Dodomp <106491543+Dodomp@users.noreply.github.com>
2024-02-17 17:02:39 +0100

    NavBar.php

---
+++ back-end/NavBar.php
@@ -0,0 +1,66 @@
+<?php
+    if (isset($_SESSION["login"]))
+    {
+        if (isset($_SESSION["admin"])){
+            echo"
+             <nav class='section__container nav__container'>
+                <a href='#' class='av__logo'>Mona </a>
+                <ul class='nav__links'>
+                    <li class='link'><a href='#'>HOME</a></li>
+                    <li class='link'><a href='#'>SHOP</a></li>
+                    <li class='link'><a href='#'>CHI SIAMO</a></li>
+                    <li class='link'><a href='#'>CARRELLO</a></li>
+                    <li class='link'><a href='#'>ADMIN AREA</a></li>
+
+                </ul>
+                <div class='nav__icons'>
+                    <span><i class='ri-shield-user-line'></i></span>
+                    <span><i class='ri-search-line'></i></span>
+                    <span><i class='ri-shopping-bag-2-line'></i></span>
+                </div>
+            </nav>
+        ";
+        }
+        else{
+            echo"
+             <nav class='section__container nav__container'>;
+                <a href='#' class='av__logo'>'Mon'sa' </a>
+                <ul class='nav__links'>
+                    <li class='link'><a href='#'>HOME</a></li>
+                    <li class='link'><a href='#'>SHOP</a></li>
+                    <li class='link'><a href='#'>CHI SIAMO</a></li>
+                    <li class='link'><a href='#'>CARRELLO</a></li>
+                    <li class='link'><a href='#'>LOG OUT</a></li>
+
+                </ul>
+                <div class='nav__icons'>
+                    <span><i class='ri-shield-user-line'></i></span>
+                    <span><i class='ri-search-line'></i></span>
+                    <span><i class='ri-shopping-bag-2-line'></i></span>
+                </div>
+            </nav>
+        ";
+        }
+
+    }
+
+    else {
+        echo"
+             <nav class='section__container nav__container'>;
+                <a href='#' class='av__logo'>Mon'sa</a>
+                <ul class='nav__links'>
+                    <li class='link'><a href='#'>HOME</a></li>
+                    <li class='link'><a href='#'>SHOP</a></li>
+                    <li class='link'><a href='#'>CHI SIAMO</a></li>
+                    <li class='link'><a href='login.php'>SIGN IN</a></li>
+                    <li class='link'><a href='registration.php'>SIGN UP</a></li>
+
+                </ul>
+                <div class='nav__icons'>
+                    <span><i class='ri-shield-user-line'></i></span>
+                    <span><i class='ri-search-line'></i></span>
+                    <span><i class='ri-shopping-bag-2-line'></i></span>
+                </div>
+            </nav>
+        ";
+    }
